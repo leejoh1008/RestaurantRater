beb79fc90f053e98fdfbbfce213aab1152624731
Johnson Lee <johnson.hwang1008@gmail.com>
2022-01-18 16:28:03 -0500

    1st commit

---
+++ RestuarantRater/Content/RestaurantDbContext.cs
@@ -0,0 +1,20 @@
+﻿using RestuarantRater.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestuarantRater.Content
+{
+    public class RestaurantDbContext : DbContext
+    {
+        public RestaurantDbContext() : base("Default Connection")
+        {
+
+        }
+
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
